689abe266e3d23d7dc38cbbf96153b81fd48bb0a
d3stroya <anabel.comino@gmail.com>
2025-11-03 13:45:46 +0100

    Tema 4 - Estructura Intro a clase String

---
+++ Tema04/Tema4/src/tema4/ClaseString.java
@@ -0,0 +1,43 @@
+package tema4;
+
+
+/**
+ * La clase String pertenece al paquete java.lang,
+ * al igual que la clase Math. Esto supone que no 
+ * tenemos que importarla para poder usarla (recuerda 
+ * que el paquete java.lang se importa automáticamente).
+ * 
+ * String es un tipo de dato que nos permite trabajar
+ * con un conjunto de caracteres (palabras o frases), 
+ * comúnmente llamadas "cadenas".
+ * Podemos guardar esos valores en variables (entre comillas dobles ""),
+ * convertirlos a mayúsculas o minúsculas, comparar
+ * dos cadenas, etc.
+ * 
+ * @author d3stroya
+ */
+public class ClaseString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declarar una variable de tipo String
+        
+        
+        // Inicializar la variable
+        
+        
+        // Declarar e inicializar una variable de tipo String
+        
+        
+        // Pedir una cadena al usuario
+        
+        
+        // Comparar dos valores ( .equals() e .equalsIgnoreCase() )
+        
+        
+        
+    }    
+
+}
