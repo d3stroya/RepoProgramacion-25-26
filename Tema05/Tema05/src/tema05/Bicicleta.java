680413921c48d98b158871e186e9fc057488f206
d3stroya <anabel.comino@gmail.com>
2025-11-10 18:25:34 +0100

    Tema 5 - Modificadores y paso de valores
    - Modificadores de acceso: public, protected, friendly y private.
    - Modificador static: atributos y métodos que pertenecen a la clase y no al objeto.
    - Paso de parámetros por valor y por referencia.

---
+++ Tema05/Tema05/src/tema05/Bicicleta.java
@@ -0,0 +1,124 @@
+package tema05;
+
+/**
+ *
+ * @author d3stroya
+ */
+public class Bicicleta {
+    // 1. ATRIBUTOS
+    // 1.1. MODIFICADORES DE ACCESO
+    int ruedas;                 // friendly -> MAL
+    protected String manillar;  // MAL
+    public String pedales;      // MAL
+    private String frenos;      // BIEEEEN!! :)
+    private boolean estaPedaleando;
+    
+    // 1.2. ATRIBUTO STATIC (ESTÁTICO) QUE PERTENECE A LA CLASE
+    private static int numBicicletas = 0;
+
+    // 2. CONSTRUCTORES
+    // 2.1. Por defecto
+    public Bicicleta() {
+        this.ruedas = 0;
+        this.manillar = "";
+        this.pedales = "";
+        this.frenos = "";
+        this.estaPedaleando = false;
+        
+        // Incrementamos el valor del atributo estático
+        incrementarNumBicicletas();
+    }
+
+    // 2.2. Parametrizado
+    public Bicicleta(int ruedas, String manillar, String pedales, String frenos, boolean estaPedaleando) {
+        this.ruedas = ruedas;
+        this.manillar = manillar;
+        this.pedales = pedales;
+        this.frenos = frenos;
+        this.estaPedaleando = estaPedaleando;
+        
+        // Incrementamos el valor del atributo estático
+        incrementarNumBicicletas();
+    } 
+
+    // 3. GETTERS Y SETTERS
+    public void setRuedas(int ruedas) {
+        this.ruedas = ruedas;
+    }
+    
+    public int getRuedas() {
+        return this.ruedas;
+    }
+    
+    public void setManillar(String manillar) {
+        this.manillar = manillar;
+    }
+    
+    public String getManillar() {
+        return this.manillar;
+    }
+    
+    public void setPedales(String pedales) {
+        this.pedales = pedales;
+    }
+    
+    public String getPedales() {
+        return this.pedales;
+    }
+    
+    public void setFrenos(String frenos) {
+        this.frenos = frenos;
+    }
+    
+    public String getFrenos() {
+        return this.frenos;
+    }
+    
+    public void setEstaPedaleando(boolean estaPedaleando) {
+        this.estaPedaleando = estaPedaleando;
+    }
+    
+    public boolean getEstaPedaleando() {
+        return this.estaPedaleando;
+    }
+    
+    /**
+     * Método estático que devuelve
+     * el número de bicicletas instanciadas (creadas).
+     * 
+     * Es un método estático porque numBicicletas
+     * es estático.
+     * 
+     * @return el número de bicicletas creadas.
+     */
+    public static int getNumBicicletas() {
+        return numBicicletas;
+    }
+
+
+    // 5. MÉTODOS
+    /**
+     * Método estático que incrementa en 1 
+     * el número de numBicicletas.
+     * 
+     * Es un método estático porque pertenece a la clase,
+     * no al objeto.
+     * 
+     * Como pertenece a la clase y no al objeto, 
+     * no usamos this, ya que this hace referencia al objeto.
+     */
+    private static void incrementarNumBicicletas() {
+        numBicicletas++;
+    }
+
+
+    // 4. TO STRING
+    @Override
+    public String toString() {
+        return "Mi bicicleta tiene " + this.ruedas + " ruedas" +
+                ", un manillar " + this.manillar + 
+                ", unos pedales " + this.pedales + 
+                ", unos frenos " + this.frenos + 
+                " y ahora ¿estoy pedaleando? " + this.estaPedaleando;
+    }
+}
