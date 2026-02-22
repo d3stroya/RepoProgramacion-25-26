17dbeea5e857274719f551c297af54cb54863f29
d3stroya <107263982+d3stroya@users.noreply.github.com>
2026-02-22 15:50:41 +0100

    Tema 10 - Tienda (plantilla)
    Ejercicio reto del tema

---
+++ Tema10/Tienda/src/tienda/Tienda.java
@@ -0,0 +1,39 @@
+
+package tienda;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author d3stroya
+ */
+public class Tienda {
+    
+     private ArrayList<Producto> productos = new ArrayList<>();
+
+    public void agregarProducto(String nombre, float precio, int stock) {
+        // IMPLEMENTAR
+    }
+
+    public ArrayList<Producto> getProductos() {
+        return productos;
+    }
+
+    public String comprarProducto(int indice, int cantidad) {
+        /*
+         Debe:
+         - Verificar stock
+         - Reducir stock si es posible
+         - Devolver mensaje de éxito o error
+        */
+        return "";
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
