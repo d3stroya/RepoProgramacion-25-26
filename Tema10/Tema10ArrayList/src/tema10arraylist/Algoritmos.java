de241415e295468a7fb84d8b12bdba0d4562eaa7
d3stroya <anabel.comino@gmail.com>
2026-03-10 17:15:08 +0100

    Tema 10 - Iteradores

--- Tema10/Tema10ArrayList/src/tema10arraylist/Algoritmos.java
+++ Tema10/Tema10ArrayList/src/tema10arraylist/Algoritmos.java
@@ -1,6 +1,7 @@
 package tema10arraylist;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 
@@ -69,6 +70,39 @@ public class Algoritmos {
         return coches.contains(coche);
     }
     
+    
+    // 5. Recorrer listas con iterador:
+    // Mostrar la lista con iterador
+    public static void mostrarConIterador(ArrayList<Coche> coches) {
+        Iterator<Coche> it = coches.iterator();
+        
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+    
+    
+    
+    // Recorrer la lista con iterador y eliminar ciertos elementos
+    public static void mostrarYEliminar(ArrayList<Coche> coches) {
+        Iterator<Coche> it = coches.iterator();
+        Coche cAux;
+        
+        while(it.hasNext()) {
+            cAux = it.next();
+            
+            if(cAux.getColor().equalsIgnoreCase("azul")) {
+                it.remove();
+            }
+            
+            System.out.println(cAux);
+            
+        }
+    }
+    
+    
+    
+    
     public static void main(String[] args) {
         ArrayList<Coche> coches = new ArrayList<>();
         Coche cocheVerde = new Coche("verde", 90);
@@ -80,19 +114,41 @@ public class Algoritmos {
             
         } while(pedirSeguir());
 //        coches.add(cocheVerde);
-        
+
+
+
+
+
+        // Mostramos la lista con iterador
+        System.out.println("\n- MOSTRANDO COCHES CON ITERADOR -");
+        mostrarConIterador(coches);
+
+        // Mostramos todos los coches y eliminamos los azules
+        System.out.println("\nEliminando coches azules...");
+        mostrarYEliminar(coches);
+
+        // Mostramos la lista con for-each
+        System.out.println("\n- MOSTRANDO COCHES -");
         mostrar(coches);
         
-        System.out.println("Mostrando coches con potencia igual o superior a...");
-        mostrarPorPotencia(coches, pedirPotencia());
         
-        System.out.println("¿La lista contiene mi coche verde de 90CV? " + buscarCoche(coches, cocheVerde));
         
-//        System.out.println("¿La lista contiene un coche de 90CV? " + buscar(coches, 90));
-
-        System.out.println("\n\nLos coches verdes son distintos:");
-        System.out.println("HashCode de cocheVerde: " + cocheVerde.hashCode());
-        System.out.println("HashCode del coche verde del arrayList: " + coches.get(2).hashCode());
+        
+        
+        
+        
+        
+        
+//        System.out.println("Mostrando coches con potencia igual o superior a...");
+//        mostrarPorPotencia(coches, pedirPotencia());
+//        
+//        System.out.println("¿La lista contiene mi coche verde de 90CV? " + buscarCoche(coches, cocheVerde));
+//        
+////        System.out.println("¿La lista contiene un coche de 90CV? " + buscar(coches, 90));
+//
+//        System.out.println("\n\nLos coches verdes son distintos:");
+//        System.out.println("HashCode de cocheVerde: " + cocheVerde.hashCode());
+//        System.out.println("HashCode del coche verde del arrayList: " + coches.get(2).hashCode());
         
     }    
 
